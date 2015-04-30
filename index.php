701e101a684298c4567f4a9b205b88876eb1165a
aliffathon <fathon.1994@gmail.com>
2015-04-30 12:13:25 +0800

    Buat file index
    Comm 1; Create Index File

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "<html>"
+echo "<head>"
+echo "Wellcome to Alif Fathon personal site"
+echo "</head>"
+echo "<body>"
+echo "You are in Alif Fathon Personal Site ^_^"
+echo "</body>"
+echo "</html>"
+?>
